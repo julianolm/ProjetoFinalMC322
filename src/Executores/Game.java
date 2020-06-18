b22f3629fc8905884db724e92464fa98f4edda8d
u_equipe_03 <julian@email.com>
2020-06-18 15:20:13 -0300

    Adicionando classes Runner e Game
    Adicionei as duas classes fornecidas pelo prof nas especificacoes do projeto

---
+++ src/Executores/Game.java
@@ -0,0 +1,20 @@
+package Executores;
+
+public class Game {
+
+	private boolean exit;
+
+	public void start() {
+		exit = false;
+		System.out.println("Game started!");
+
+		while (!exit) {
+			drawBoard();
+			readInput();
+			updateBoard();
+		}
+
+		System.out.println("Game terminated. Bye!");
+
+	}
+}
---
+++ src/Executores/Runner.java
@@ -0,0 +1,11 @@
+package Executores;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Game g = new Game();
+		g.start();
+
+	}
+}
