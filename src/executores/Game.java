f9534008f25649e83dd9943555d92b8b0839cdf2
u_equipe_03 <julian@email.com>
2020-07-22 18:31:48 -0300

    updates

--- src/executores/Game.java
+++ src/executores/Game.java
@@ -1,20 +1,50 @@
 package executores;
 
-public class Game {
+import personagens.*;
+import personagens.jogaveis.*;
 
+import java.util.Scanner;
+
+public class Game {
+	
 	private boolean exit;
+	private Jogavel heroi;
+	
+	Scanner input = new Scanner(System.in);
 
 	public void start() {
 		exit = false;
-		System.out.println("Game started!");
+		System.out.println("Game started!\n");
+		
+		initialConfig(); //esse metodo recebe as entradas iniciais que determinam o tipo do heroi e seu nome
 
 		while (!exit) {
 			//drawBoard();
 			//readInput();
 			//updateBoard();
+			exit=true; // so p rodar e ver se ta funcionando
 		}
 
 		System.out.println("Game terminated. Bye!");
 
 	}
+	
+	private void initialConfig() {
+		System.out.print("Com qual Heroi Voce quer Jogar? (Digite uma letra e precione enter)\n");
+		System.out.print("Anao - D\nBarbaro - B\nElfo - E\nFeiticeiro - W\n");
+		String letra = input.nextLine();
+		
+		System.out.print("Escolha o nome do seu heroi: ");
+		String nome = input.nextLine();
+		
+		if (letra.equals("D"))
+			heroi = new Anao(nome);
+		else if (letra.contentEquals("B"))
+			heroi = new Barbaro(nome);
+		else if (letra.contentEquals("E"))
+			heroi = new Elfo(nome);
+		else if (letra.contentEquals("W"))
+			heroi = new Feiticeiro(nome);	
+	}
+	
 }
